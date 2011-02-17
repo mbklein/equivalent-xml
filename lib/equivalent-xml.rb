503f9d2510e6abcb6d4bebe37019c4acd69d2953
Michael Klein <mbklein@gmail.com>
2011-02-17 14:03:12 -0800

    Added optional block for yielding evaluated nodes

--- lib/equivalent-xml.rb
+++ lib/equivalent-xml.rb
@@ -1,61 +1,63 @@
 module EquivalentXml
   
-  class << self
+  ELEMENT_NODE                = 1
+  ATTRIBUTE_NODE              = 2
+  TEXT_NODE                   = 3
+  CDATA_SECTION_NODE          = 4
+  ENTITY_REFERENCE_NODE       = 5
+  ENTITY_NODE                 = 6
+  PROCESSING_INSTRUCTION_NODE = 7
+  COMMENT_NODE                = 8
+  DOCUMENT_NODE               = 9
+  DOCUMENT_TYPE_NODE          = 10
+  DOCUMENT_FRAGMENT_NODE      = 11
+  NOTATION_NODE               = 12
 
-    ELEMENT_NODE                = 1
-    ATTRIBUTE_NODE              = 2
-    TEXT_NODE                   = 3
-    CDATA_SECTION_NODE          = 4
-    ENTITY_REFERENCE_NODE       = 5
-    ENTITY_NODE                 = 6
-    PROCESSING_INSTRUCTION_NODE = 7
-    COMMENT_NODE                = 8
-    DOCUMENT_NODE               = 9
-    DOCUMENT_TYPE_NODE          = 10
-    DOCUMENT_FRAGMENT_NODE      = 11
-    NOTATION_NODE               = 12
+  class << self
     
     DEFAULT_OPTS                = { :element_order => false, :normalize_whitespace => true }
 
-    def equivalent?(node_1, node_2, opts = {})
+    def equivalent?(node_1, node_2, opts = {}, &block)
       opts = DEFAULT_OPTS.merge(opts)
-      self.compare_nodes(node_1, node_2, opts)
+      self.compare_nodes(node_1, node_2, opts, &block)
     end
   
-    def compare_nodes(node_1, node_2, opts)
+    def compare_nodes(node_1, node_2, opts, &block)
+      yield(node_1, node_2) if block_given?
+      
       if (node_1.class != node_2.class) or self.same_namespace?(node_1,node_2) == false
         false
       else
         case node_1.node_type
         when DOCUMENT_NODE
-          self.compare_documents(node_1,node_2,opts)
+          self.compare_documents(node_1,node_2,opts,&block)
         when ELEMENT_NODE
-          self.compare_elements(node_1,node_2,opts)
+          self.compare_elements(node_1,node_2,opts,&block)
         when ATTRIBUTE_NODE
-          self.compare_attributes(node_1,node_2,opts)
+          self.compare_attributes(node_1,node_2,opts,&block)
         when CDATA_SECTION_NODE
-          self.compare_cdata(node_1,node_2,opts)
+          self.compare_cdata(node_1,node_2,opts,&block)
         when TEXT_NODE
-          self.compare_text(node_1,node_2,opts)
+          self.compare_text(node_1,node_2,opts,&block)
         else
-          self.compare_children(node_1,node_2,opts)
+          self.compare_children(node_1,node_2,opts,&block)
         end
       end
     end
 
-    def compare_documents(node_1, node_2, opts)
-      self.equivalent?(node_1.root,node_2.root,opts)
+    def compare_documents(node_1, node_2, opts, &block)
+      self.equivalent?(node_1.root,node_2.root,opts,&block)
     end
     
-    def compare_elements(node_1, node_2, opts)
-      (node_1.name == node_2.name) && self.compare_children(node_1,node_2,opts)
+    def compare_elements(node_1, node_2, opts, &block)
+      (node_1.name == node_2.name) && self.compare_children(node_1,node_2,opts,&block)
     end
     
-    def compare_attributes(node_1, node_2, opts)
+    def compare_attributes(node_1, node_2, opts, &block)
       (node_1.name == node_2.name) && (node_1.value == node_2.value)
     end
     
-    def compare_text(node_1, node_2, opts)
+    def compare_text(node_1, node_2, opts, &block)
       if opts[:normalize_whitespace]
         node_1.text.strip.gsub(/\s+/,' ') == node_2.text.strip.gsub(/\s+/,' ')
       else
@@ -63,11 +65,11 @@ module EquivalentXml
       end
     end
     
-    def compare_cdata(node_1, node_2, opts)
+    def compare_cdata(node_1, node_2, opts, &block)
       node_1.text == node_2.text
     end
     
-    def compare_children(node_1, node_2, opts)
+    def compare_children(node_1, node_2, opts, &block)
       ignore_proc = lambda do |child|
         child.is_a?(Nokogiri::XML::Comment) ||
         child.is_a?(Nokogiri::XML::ProcessingInstruction) ||
@@ -86,7 +88,7 @@ module EquivalentXml
       result
     end
     
-    def compare_nodesets(nodeset_1, nodeset_2, opts)
+    def compare_nodesets(nodeset_1, nodeset_2, opts, &block)
       local_set_1 = nodeset_1.dup
       local_set_2 = nodeset_2.dup
       
@@ -95,7 +97,7 @@ module EquivalentXml
       end
     
       local_set_1.each do |search_node|
-        found_node = local_set_2.find { |test_node| self.equivalent?(search_node,test_node,opts) }
+        found_node = local_set_2.find { |test_node| self.equivalent?(search_node,test_node,opts,&block) }
         if found_node.nil?
           return false
         else
